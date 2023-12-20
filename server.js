9420c16cfe52d4f3c81aa6b89750b53450669f5a
Avinaw Kumar <0811kumaravinaw@gmail.com>
2023-12-20 21:12:35 +0530

    server side and clint side connection

--- server.js
+++ server.js
@@ -16,7 +16,15 @@ io.on("connection", function(socket){
 		socket.join(data.uid);
 		socket.in(data.sender_uid).emit("init", data.uid);
 	});
-	
+	socket.on("file-meta",function(data){
+		socket.in(data.uid).emit("fs-meta", data.metadata);
+	});
+	socket.on("fs-start",function(data){
+		socket.in(data.uid).emit("fs-share", {});
+	});
+	socket.on("file-raw",function(data){
+		socket.in(data.uid).emit("fs-share", data.buffer);
+	})
 });
 
 server.listen(5000);
